8760a608d512bb102184589d81c0ca98b3fc8907
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2024-01-23 22:20:26 +0100

    .

---
+++ Dockerfile/ENTRYPOINT/exec-form/run.sh
@@ -0,0 +1,27 @@
+docker rm $NAME-container > /dev/null
+
+msg() { echo -e "\n\e[94m$*\e[0m" ;}
+
+NAME=entrypoint-exec-test
+
+msg docker build
+docker build -q -t $NAME-image .
+
+msg docker create
+docker create --name $NAME-container  $NAME-image > /dev/null
+
+msg Starting the container for the first time:
+docker start -i      $NAME-container
+sleep 1
+
+msg Starting the container the second time:
+docker start -i      $NAME-container
+sleep 1
+
+msg Starting the container the third time:
+docker start -i      $NAME-container
+sleep 1
+
+
+msg docker history
+docker history $NAME-image
