00a38bb1cf5256a9c0e59e3609e8291c0d9d8566
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2024-01-25 23:07:21 +0100

    .

---
+++ commands/compose/busybox-webserver/run.sh
@@ -0,0 +1,7 @@
+echo Use 'docker compose up'
+
+# docker build -t tq84/http-server -f Dockerfile-server .
+
+# docker run --rm --name http-server-container -p 8422:8422 tq84/http-server
+# docker run --rm --name http-server-container -p 8422:8422 tq84/http-server
+
